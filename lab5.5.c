39829ef3dc70fe37535ccc627de034f860ca4297
Hreaun <43428176+Hreaun@users.noreply.github.com>
2018-11-13 22:20:52 +0700

    Update lab5.5.c

--- lab5.5.c
+++ lab5.5.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,47 +7,47 @@
 
 void swap(int *a, int i, int j)
 {
-    int temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+	int temp = a[i];
+	a[i] = a[j];
+	a[j] = temp;
 }
 
-void heap(int *a, int N )
+void heap(int *a, int N)
 {
-    for (int i = N / 2 - 1; i >= 0; i--)
-    {
-        if ((a[i] > a[i * 2 + 1]) && (i * 2 + 1 < N))
-            if  ((a[i] > a[i * 2 + 2]) && (i * 2 + 2 < N))
-                swap(a, i, i * 2 + 2);
-            else
-                swap(a, i, i * 2 + 1);
-        else
-            if ((a[i] > a[i * 2 + 2]) && (i * 2 + 2 < N))
-            swap(a, i, i * 2 + 2);
-    }
+	for (int i = N / 2 - 1; i >= 0; i--)
+	{
+		if ((a[i] > a[i * 2 + 1]) && (i * 2 + 1 < N))
+			if ((a[i] > a[i * 2 + 2]) && (a[i * 2 + 2] < a[i * 2 + 1]) && (i * 2 + 2 < N))
+				swap(a, i, i * 2 + 2);
+			else
+				swap(a, i, i * 2 + 1);
+		else
+			if ((a[i] > a[i * 2 + 2]) && (i * 2 + 2 < N))
+				swap(a, i, i * 2 + 2);
+	}
 }
 
 void sort(int *a, int N)
 {
-    heap(a, N);
-    for (int i = N - 1; i >= 1; i--)
-    {
-        swap(a, 0, i);
-        heap(a, i);
-    }
+	heap(a, N);
+	for (int i = N - 1; i >= 1; i--)
+	{
+		swap(a, 0, i);
+		heap(a, i);
+	}
 }
 
 int main()
 {
-    int *a;
-    int N;
-    scanf("%d", &N);
-    a = (int*)malloc(N * sizeof(int));
-    for (int i = 0; i < N; i++)
-        scanf("%d", &a[i]);
-    sort(a, N);
-    for (int i = N - 1; i >= 0; i--)
-        printf("%d ", a[i]);
-    free(a);
-    return 0;
+	int *a;
+	int N;
+	scanf("%d", &N);
+	a = (int*)malloc(N * sizeof(int));
+	for (int i = 0; i < N; i++)
+		scanf("%d", &a[i]);
+	sort(a, N);
+	for (int i = N - 1; i >= 0; i--)
+		printf("%d ", a[i]);
+	free(a);
+	return 0;
 }
