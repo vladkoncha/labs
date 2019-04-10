b687255157cda0774b0b8d7d02bf7f032f73aea3
Hreaun <43428176+Hreaun@users.noreply.github.com>
2019-04-10 22:09:26 +0700

    Update Dijkstra.c

--- SecondSem/Dijkstra.c
+++ SecondSem/Dijkstra.c
@@ -2,109 +2,36 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
-#define CHECKED 1
-typedef struct vector vector;
+typedef short bool;
 
-struct array {
-	int vertex;
-	long long weight;
-};
-
-struct vector {
-	int mem;
-	struct array *array;
-	int size;
-
-	int(*begin)(vector);
-
-	int(*end)(vector);
-
-	void(*pushBack)(vector *, int, long long);
-
-	void(*remov)(vector *);
-};
-
-void pushBack(vector *vector, int value, long long edgeWeight) {
-	if (vector->size >= vector->mem) {
-		vector->array =
-			realloc(vector->array, sizeof(struct array) * vector->size * 2);
-		vector->mem *= 2;
+void Way(int **graph, const long long *marker, int F, long long weight, int N) {
+	int *way = (int *)calloc(N, (sizeof(int)));
+	way[0] = F + 1;
+	int k = 1;
+	while (weight > 0) {
+		for (int i = 0; i < N; i++) {
+			if (graph[F][i] != 0) {
+				if ((weight - graph[F][i]) == marker[i]) {
+					weight = weight - graph[F][i];
+					F = i;
+					way[k] = i + 1;
+					k++;
+				}
+			}
+		}
 	}
-	vector->array[vector->size].vertex = value;
-	vector->array[vector->size].weight = edgeWeight;
-	vector->size++;
+	for (int i = 0; i < k; i++)
+		printf("%d ", way[i]);
+	free(way);
 }
 
-int begin(vector vector) { return vector.array[0].vertex; }
-
-int end(vector vector) {
-	if (vector.size > 0)
-		return vector.array[vector.size - 1].vertex;
-	return 0;
-}
-
-void remov(vector *vector) {
-	if (vector->size > 0) {
-		vector->array[--vector->size].vertex = 0;
-		vector->array[vector->size].weight = 0;
+void FreeGraph(int **graph, int N) {
+	for (int i = 0; i < N; i++) {
+		free(graph[i]);
 	}
-}
-
-struct vector NewVector() {
-	vector buff = {
-		1, malloc(sizeof(struct array)), 0, &begin, &end, &pushBack, &remov };
-	return buff;
-}
-
-void FreeGraph(struct vector *graph, int N) {
-	for (int i = 0; i < N; ++i)
-		free(graph[i].array);
 	free(graph);
-}
-
-int overflowCounter = 0;
-void Dijkstra(vector *graph, vector *marker, int N, int S, int F) {
-	for (int i = 0; i < graph[S].size; i++) {
-		if (graph[S].array[i].weight < marker->array[graph[S].array[i].vertex].weight)
-			marker->array[graph[S].array[i].vertex].weight = graph[S].array[i].weight;
-	}
-	marker->array[S].vertex = CHECKED;
-	int checked = 0;
-	long long *vertexWeight = NULL;
-	for (int currentVertex = 0; currentVertex < N; currentVertex++) {
-		if (currentVertex == S)
-			continue;
-		for (int adjVertex = 0; adjVertex < graph[currentVertex].size; adjVertex++) {
-			if (currentVertex == graph[currentVertex].array[adjVertex].vertex)
-				continue;
-			checked = (marker->array[graph[currentVertex].array[adjVertex].vertex].vertex == CHECKED);
-			vertexWeight = &marker->array[graph[currentVertex].array[adjVertex].vertex].weight;
-			if (currentVertex == F) {
-				if (((graph[currentVertex].array[adjVertex].weight + marker->array[currentVertex].weight) > INT_MAX)
-					|| (graph[currentVertex].array[adjVertex].weight > INT_MAX)
-					|| (marker->array[currentVertex].weight > INT_MAX))
-					overflowCounter++;
-			}
-			if ((!checked) && (graph[currentVertex].array[adjVertex].weight + marker->array[currentVertex].weight < *vertexWeight)) {
-				*vertexWeight = graph[currentVertex].array[adjVertex].weight + marker->array[currentVertex].weight;
-			}
-		}
-		if (marker->array[currentVertex].weight != UINT_MAX)
-			marker->array[currentVertex].vertex = CHECKED;
-	}
-}
-
-void Way(vector *graph, vector marker, int S, int currentVertex) {
-	for (int i = 0; marker.array[currentVertex].weight != 0; i++) {
-		if ((marker.array[currentVertex].weight - graph[currentVertex].array[i].weight)
-			== marker.array[graph[currentVertex].array[i].vertex].weight) {
-			printf("%d ", currentVertex + 1);
-			Way(graph, marker, S, graph[currentVertex].array[i].vertex);
-			return;
-		}
-	}
-	printf("%d", S + 1);
 }
 
 int main() {
@@ -140,18 +67,18 @@ int main() {
 		exit(0);
 	}
 
-	vector *graph;
-	graph = malloc(sizeof(vector) * N);
-	for (int i = 0; i < N; ++i)
-		graph[i] = NewVector();
+	int **graph = (int **)malloc(N * sizeof(int *));
+	for (int i = 0; i < N; i++) {
+		graph[i] = calloc(N, sizeof(int));
+	}
 
 	int edgeStart = 0;
 	int edgeEnd = 0;
-	long long edgeWeight = 0;
+	int edgeWeight = 0;
 	for (int i = 0; i < M; i++) {
 		edgeStart = edgeEnd = 0;
 		edgeWeight = 0;
-		scanf("%d %d %lld", &edgeStart, &edgeEnd, &edgeWeight);
+		scanf("%d %d %d", &edgeStart, &edgeEnd, &edgeWeight);
 		if ((edgeEnd == 0) || (edgeStart == 0) || (edgeWeight == 0)) {
 			printf("bad number of lines");
 			FreeGraph(graph, N);
@@ -167,37 +94,71 @@ int main() {
 			FreeGraph(graph, N);
 			exit(0);
 		}
-		graph[edgeStart - 1].pushBack(&graph[edgeStart - 1], edgeEnd - 1, edgeWeight);
-		graph[edgeEnd - 1].pushBack(&graph[edgeEnd - 1], edgeStart - 1, edgeWeight);
+		graph[edgeStart - 1][edgeEnd - 1] = edgeWeight;
+		graph[edgeEnd - 1][edgeStart - 1] = edgeWeight;
 	}
 
-	vector marker;
-	marker = NewVector();
+	long long *marker = (long long *)calloc(N, (sizeof(long long)));
+	bool *check = (bool *)calloc(N, (sizeof(bool)));
+
 	for (int i = 0; i < N; i++) {
-		marker.pushBack(&marker, 0, UINT_MAX);
+		marker[i] = LLONG_MAX;
+		check[i] = 1;
 	}
-	marker.array[S - 1].weight = 0;
+	marker[S - 1] = 0;
 
-	Dijkstra(graph, &marker, N, S - 1, F - 1);
+	long long minWeight = 0;
+	long long minVertex = 0;
 
-	for (int i = 0; i < N; i++)
-		if (marker.array[i].weight == UINT_MAX)
-			printf("oo ");
-		else if (marker.array[i].weight > INT_MAX)
-			printf("INT_MAX+ ");
-		else
-			printf("%lld ", marker.array[i].weight);
+	do {
+		minVertex = LLONG_MAX;
+		minWeight = LLONG_MAX;
+		for (int i = 0; i < N; i++) {
+			if (check[i] && marker[i] < minWeight) {
+				minWeight = marker[i];
+				minVertex = i;
+			}
+		}
 
+		if (minVertex != LLONG_MAX) {
+			for (int i = 0; i < N; i++) {
+				if (graph[minVertex][i] > 0) {
+					if ((minWeight + graph[minVertex][i]) < marker[i]) {
+						marker[i] = minWeight + graph[minVertex][i];
+					}
+				}
+			}
+			check[minVertex] = 0;
+		}
+	} while (minVertex < LLONG_MAX);
+
+	int overflowCounter = 0;
+	for (int i = 0; i < N; i++) {
+		if (marker[i] != LLONG_MAX) {
+			if (marker[i] >= INT_MAX) {
+				overflowCounter++;
+			}
+			if (marker[i] > INT_MAX) {
+				printf("INT_MAX+ ");
+			}
+			else printf("%lld ", marker[i]);
+		}
+		else printf("oo ");
+	}
 	printf("\n");
 
-	if (marker.array[F - 1].weight == UINT_MAX)
+	long long weight = marker[F - 1];
+	if (weight == LLONG_MAX) {
 		printf("no path");
-	else if ((marker.array[F - 1].weight > INT_MAX) && (overflowCounter >= 2))
+	}
+	else if (weight > INT_MAX && overflowCounter > 2) {
 		printf("overflow");
+	}
 	else
-		Way(graph, marker, S - 1, F - 1);
+		Way(graph, marker, F - 1, weight, N);
 
 	FreeGraph(graph, N);
-	free(marker.array);
+	free(marker);
+	free(check);
 	return 0;
 }
