787cdc130e6241317600f521660ea72ef50ea7bd
Hreaun <43428176+Hreaun@users.noreply.github.com>
2019-04-24 20:49:28 +0700

    Update base64,0.c

--- SecondSem/base64,0.c
+++ SecondSem/base64,0.c
@@ -6,35 +6,44 @@ int main() {
   FILE *out = fopen("out.txt", "w");
   char alphabet[] =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-  unsigned char a[3] = { 0 };
-  unsigned char x[4] = { 0 };
+  unsigned char a[3] = {0};
+  unsigned char x[4] = {0};
+  int c = 0;
   int counter = 0;
+  int lastChar = 0;
 
-  while (!feof(in)){ //add odd number
+  while ((c = fgetc(in)) != EOF) { // add odd number
     counter = 1;
-    a[0] = fgetc(in);
+    a[0] = c;
     x[0] = a[0] & 252u;
     x[0] >>= 2u;
 
-    if (!feof(in)) {
-      counter++;
-      a[1] = fgetc(in);
-      x[1] = ((a[0] & 3u) << 4u) | (a[1] >> 4u);
+    if ((c = fgetc(in)) == EOF) {
+      a[1] = 0;
+      lastChar = 1;
+    } else
+      a[1] = c;
+    counter++;
+    x[1] = ((a[0] & 3u) << 4u) | (a[1] >> 4u);
 
-      if (!feof(in)) {
-        counter += 2;
-        a[2] = fgetc(in);
-        x[2] = ((a[1] & 15u) << 2u) | (a[2] >> 6u);
-        x[3] = a[2] & 63u;
-      }
+    if (lastChar != 1) {
+      if ((c = fgetc(in)) == EOF) {
+        a[2] = 0;
+        lastChar = 2;
+      } else
+        a[2] = c;
+      counter += 2;
+      x[2] = ((a[1] & 15u) << 2u) | (a[2] >> 6u);
+      x[3] = a[2] & 63u;
     }
 
+    if (lastChar == 2)
+      counter--;
     for (int i = 0; i < counter; i++) {
       fprintf(out, "%c", alphabet[x[i]]);
       x[i] = 0;
     }
   }
-
 
   fclose(in);
   fclose(out);
